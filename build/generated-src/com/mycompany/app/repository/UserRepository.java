421abd1c9769435465b5ab46ff47f0f9aef26999
Siva Reddy Thunguti <sthunguti@manh.com>
2026-03-29 17:49:41 +0530

    removed build files

--- build/generated-src/com/mycompany/app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mycompany.app.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
